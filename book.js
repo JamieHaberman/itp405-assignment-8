7f6582f896cf3d362fb58f09628e4baff08afe40
Jamie Haberman <jahaberm@usc.edu>
2017-04-18 09:19:18 -0700

    “submitted”

---
+++ book.js
@@ -0,0 +1,16 @@
+var bookshelf = require('./bookshelf.js');
+var Author = require('./author');
+var Publisher = require('./publisher');
+
+var Book = bookshelf.Model.extend({
+	tableName: 'books',
+
+	author: function() {
+		return this.belongsTo(Author);
+	},
+	publisher: function() {
+		return this.belongsTo(Publisher);
+	}
+});
+
+module.exports = Book;
